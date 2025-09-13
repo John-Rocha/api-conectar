348135a22ec1fab4564c0a6f75bd4295663081bd
Johnathan Rocha <johnathanrocha@gmail.com>
2025-09-13 11:17:30 -0300

    feat: restructure application by removing AppController and AppService, and adding Auth and User modules with respective controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { UserModule } from './user/user.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule],
+  controllers: [AuthController, UserController],
+  providers: [AuthService, UserService],
 })
 export class AppModule {}
